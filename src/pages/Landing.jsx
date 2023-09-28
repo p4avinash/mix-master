a286b15f51ff399e124218022d0984422780cd2c
p4avinash <p4avinashkumar@gmail.com>
2023-09-28 11:10:34 +0530

    implemented react query to cache the data

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,16 +7,35 @@ import SearchForm from "../components/SearchForm"
 const cocktailSearchUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
 
-export const loader = async ({ request }) => {
-  const url = new URL(request.url)
+import { useQuery } from "@tanstack/react-query"
 
-  const searchTerm = url.searchParams.get("search") || ""
-  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`)
-  return { drinks: response.data.drinks, searchTerm }
+const searchCocktailsQuery = (searchTerm) => {
+  return {
+    queryKey: ["search", searchTerm || "all"],
+    queryFn: async () => {
+      const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`)
+      return response.data.drinks
+    },
+  }
 }
 
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
+    const url = new URL(request.url)
+
+    const searchTerm = url.searchParams.get("search") || ""
+    // const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`)
+    // return { drinks: response.data.drinks, searchTerm }
+
+    await queryClient.ensureQueryData(searchCocktailsQuery(searchTerm))
+    return { searchTerm }
+  }
+
 const Landing = () => {
-  const { drinks, searchTerm } = useLoaderData()
+  // const { drinks, searchTerm } = useLoaderData()
+  const { searchTerm } = useLoaderData()
+  const { data: drinks } = useQuery(searchCocktailsQuery(searchTerm))
 
   return (
     <>
